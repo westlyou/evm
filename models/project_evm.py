2cca13420d0a91c70bb948eac07e6b2b1e86f87f
Juan Carlos Falcón Portillo <carlos.falk@gmail.com>
2017-04-03 19:30:53 +0100

    Form type tab of EVM indicators with changes in design Changes in class; Include name and date of indicator registration

--- models/project_evm.py
+++ models/project_evm.py
@@ -30,8 +30,11 @@ _TIPO_INDICADOR = [('PV', 'Valor planificado (Planned Value)'),
 class project_evm(models.Model):
     _name = 'project.evm'
     _description = 'Indicadores para controlar el valor ganado de los proyectos'
+    _inherit = ['mail.thread']
 
+    name = fields.Char('Descripción', required=True, copy=False, help='')
     project_id = fields.Many2one('project.project', string='Proyecto', required=True, copy=False, help='Proyecto al cual esta asociado el indicador')
-    tipo_indicador = fields.Selection(_TIPO_INDICADOR, 'Tipo', required=True, copy=False, help ='Tipo de indicador EVM')
+    tipo_indicador = fields.Selection(_TIPO_INDICADOR, 'Tipo', required=True, copy=False, help='Tipo de indicador EVM')
     valor_indicador = fields.Float('Valor', copy=False, help='Valor del indicador EVM')
-    observaciones = fields.Text('Observaciones')
+    fecha_registro = fields.Date('', required=True, copy=False, help='Fecha de registro del indicador')
+    observaciones = fields.Text('Observaciones', copy=False, help='Comentarios de interes acerca de la anotación del valor del indicador')
