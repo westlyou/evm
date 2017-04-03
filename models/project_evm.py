f5499f94c484aeeab0881ddb27035ab0c4988a32
Juan Carlos Falcón Portillo <carlos.falk@gmail.com>
2017-04-03 19:52:25 +0100

    Enable the ability to copy indicator

--- models/project_evm.py
+++ models/project_evm.py
@@ -32,9 +32,9 @@ class project_evm(models.Model):
     _description = 'Indicadores para controlar el valor ganado de los proyectos'
     _inherit = ['mail.thread']
 
-    name = fields.Char('Descripción', required=True, copy=False, help='')
-    project_id = fields.Many2one('project.project', string='Proyecto', required=True, copy=False, help='Proyecto al cual esta asociado el indicador')
-    tipo_indicador = fields.Selection(_TIPO_INDICADOR, 'Tipo', required=True, copy=False, help='Tipo de indicador EVM')
+    name = fields.Char('Descripción', required=True, copy=False, help='Nombre del proceso que ha generado el indicador',default='Indicador')
+    project_id = fields.Many2one('project.project', string='Proyecto', required=True, help='Proyecto al cual esta asociado el indicador')
+    tipo_indicador = fields.Selection(_TIPO_INDICADOR, 'Tipo', required=True, help='Tipo de indicador EVM')
     valor_indicador = fields.Float('Valor', copy=False, help='Valor del indicador EVM')
-    fecha_registro = fields.Date('', required=True, copy=False, help='Fecha de registro del indicador')
+    fecha_registro = fields.Date('', required=True, help='Fecha de registro del indicador')
     observaciones = fields.Text('Observaciones', copy=False, help='Comentarios de interes acerca de la anotación del valor del indicador')
